13e9f321d4bc618675c3f4fa7de7b977ff8df940
Jake Soenneker <jake@soenneker.com>
2026-06-01 16:09:01 -0500

    Initial

---
+++ test/Soenneker.Bunny.OpenApiClient.Tests/BunnyOpenApiClientTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.HostedUnit;
+
+namespace Soenneker.Bunny.OpenApiClient.Tests;
+
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class BunnyOpenApiClientTests : HostedUnitTest
+{
+    public BunnyOpenApiClientTests(Host host) : base(host)
+    {
+    }
+
+    [Test]
+    [Skip("Manual")]
+    public void Default()
+    {
+
+    }
+}
